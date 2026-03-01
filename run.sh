ab37dde2f0257daf77ad084c983bcdbfb6e94162
Eric Shau <eshau00@gmail.com>
2026-03-01 11:43:14 -0500

    del shit viz

--- run.sh
+++ run.sh
@@ -1,15 +1,21 @@
+#!/bin/sh
+
+source ~/.bashrc
+conda activate es
+
 CUDA_VISIBLE_DEVICES=0 python -m train.train_diffusion \
-  --tfrecord_path /data/datasets/waymo/waymo-open-dataset-v1.3.1/tf_example/training/training_tfexample.tfrecord@1000 \
+  --tfrecord_path /zfsauton/datasets/WOMD/tf_example/tf_example/training/training_tfexample.tfrecord@1000 \
   --seed 42 \
-  --batch_size 1536 \
-  --shuffle_buffer_size 1536 \
+  --batch_size 2048 \
+  --shuffle_buffer_size 2048 \
   --dataset_num_shards 2 \
   --epochs 500 \
-  --steps_per_epoch 65 \
-  --save_every 10 \
-  --save_dir ./train/checkpoints \
+  --steps_per_epoch 50 \
+  --save_every 100 \
+  --save_dir /zfsauton/scratch/eshau/checkpoints \
   --warmup_steps 2000 \
-  --ema_update_every 1 \
+  --lr 0.00001 \
+  --ema_update_every 4 \
   --hidden_dim 256 \
   --cond_dim 256 \
   --target_dim 5 \
--- visualization/__init__.py
+++
@@ -1,4 +0,0 @@
-from visualization.rasterization import get_raster_from_simulator_state_with_agents
-
-__all__ = ["get_raster_from_simulator_state_with_agents"]
-
